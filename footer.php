1cf2c390e0480316387e893b88381bd1ad52700d
Kaiza Ilomo <kunonu@protonmail.com>
2021-08-08 15:48:31 +0300

    Change footer starting year to 2007, the year the school was founded

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@ standard footer for all web pages
         <?php
 
         echo "<p id=\"standardFooter\" > <strong>Some rights reserved
-<br>Copyright &copy;2021-" . date("Y") ."<br>". "
+<br>Copyright &copy;2007-" . date("Y") ."<br>". "
  Ingeniously crafted with much love by <a href=\"https://github.com/iloloheroes/\"> Code for Mang'oto </a>  </strong> </p>";
         ?>
     </body>
