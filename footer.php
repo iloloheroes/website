79b2a60453f46e80c366c4f943a155f58aa9f99b
Kaiza Ilomo <kunonu@protonmail.com>
2021-08-13 11:55:18 +0300

    Modify Index

--- footer.php
+++ footer.php
@@ -4,17 +4,19 @@ standard footer for all web pages
 -->
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="styles/styles.css">
+        <link href="styles/styles.css" rel="stylesheet" >
         <meta charset="UTF-8">
         <title> Standard Footer </title>
 
     </head>
     <body>
         <?php
+        echo "<p id=\"standardFooter\" > <strong> Some rights reserved <br>
+        Copyright &copy;2007-" . date("Y") . " </p> <br>" ?>
+ Ingeniously crafted with much love by
+ <a href=\"https://github.com/iloloheroes/\" target="_blank"> Code for Mang'oto
 
-        echo "<p id=\"standardFooter\" > <strong>Some rights reserved
-<br>Copyright &copy;2007-" . date("Y") ."<br>". "
- Ingeniously crafted with much love by <a href=\"https://github.com/iloloheroes/\"> Code for Mang'oto </a>  </strong> </p>";
-        ?>
+        <img src = "/images/socialmedia/github.png" alt = "Github Logo"
+             style = "width:42px; height:42px; border:0;">
     </body>
 </html>
--- index.php
+++ index.php
@@ -2,40 +2,54 @@
 <html>
 
 <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>
 Mang'oto Secondary School
 </title>
 </head>
 
 <body>
-<h1>Welcome!</h1>
+  <h1> Mang'oto Secondary School </h1>
+  <h2> MOTTO: EDUCATION IS EMPOWERMENT </h2>
+
+<h3> Welcome! </h3>
 <p> This is Mang'oto Secondary School website. </p>
-<p> <a href="http://www.facebook.com/iloloheroes/"> Join us on Facebook</a></p>
+<p> <a href="https://www.facebook.com/iloloheroes/"> Join us on Facebook</a></p>
+<acronym title="Science, Technology, Engineering and Mathematics"> STEM </acronym>
 
 <hr>
 
 <br/><a href="contacts.php/"> <strong> Contact us </strong></a>
 
-<p> Connect with <strong> Mang'oto Secondary School&#01136</strong>on  <strong>our social media</strong></p>
+<p> Connect with <strong> Mang'oto Secondary School </strong> on <strong> our social media</strong></p>
 
+<table>
 
-<p><a href = "https://www.facebok.com/iloloheroes/" target = "_blank">
-        <img src="/images/socialmedia/facebook.png" alt="Facebook logo"
-             style="width:42px;height:42px;border:0;"><strong>Ungana nasi Facebook</strong></a></p>
-<p><a href = "https://www.twitter.com/iloloheroes/" target = "_blank">
-        <img src="/images/socialmedia/twitter.png" alt="Twitter logo"
-             style="width:42px;height:42px;border:0;"> <strong>Tufuatilie Twitter</strong></a></p>
-<p><a href = "https://www.instagram.com/iloloheroes/" target="_blank">
-        <img src="/images/socialmedia/instagram.jpg" alt="Instagram Logo"
-             style="width:42px;height:42px;border:0;"><strong>Tufuatilie Instagram </strong></a></p>
-<p><a href = "https://www.linkedin.com/in/iloloheroes/" target="_blank">
-        <img src="/images/socialmedia/linkedin.png" alt = "Linkedln Logo"
-             style="width:42px;height:42px;border:0;"> <strong>Panua wigo wako wa ajira nasi LinkedIn</strong> </a></p>
-<p><a href = "https://www.github.com/iloloheroes/" target = "_blank" >
-        <img src = "/images/socialmedia/github.png" alt = "Github Logo"
-             style = "width:42px; height:42px; border:0;"><strong>Chungulia miradi yetu ya Kiteknolojia Github</strong></a> </p>
+<tr>
+<td>
+  <a href = "https://www.facebok.com/iloloheroes/" target = "_blank">
+          <img src="/images/socialmedia/facebook.png" alt="Facebook logo"
+               style="width:42px;height:42px;border:0;">
+</td>
+<td>
+  <a href = "https://www.twitter.com/iloloheroes/" target = "_blank">
+          <img src="/images/socialmedia/twitter.png" alt="Twitter logo"
+               style="width:42px;height:42px;border:0;">
+</td>
+<td>
+  <a href = "https://www.instagram.com/iloloheroes/" target="_blank">
+          <img src="/images/socialmedia/instagram.jpeg" alt="Instagram Logo"
+               style="width:42px;height:42px;border:0;">
+</td>
+<td>
+  <a href = "https://www.linkedin.com/in/iloloheroes/" target="_blank">
+          <img src="/images/socialmedia/linkedin.png" alt = "Linkedln Logo"
+               style="width:42px;height:42px;border:0;">
+</td>
 
-
+</tr>
+</table>
 <?php
 
 include_once 'footer.php';
